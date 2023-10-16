575e7c72b55f530164918ced07714bc3dd40c2c8
juanpinheirx <jroliveirapinheiro@gmail.com>
2023-10-16 05:38:26 -0300

    type annotations with functions

--- day01/index.ts
+++ day01/index.ts
@@ -2,3 +2,18 @@
 const user: string = 'Juan';
 const age: number = 26;
 const italian: boolean = true // italian wannabe
+
+// now with functions
+
+// this function gets a string as parameter, and with ts i'm able to guarantee this type.
+function greet(name: string) {
+  console.log(`Olá, ${name.toUpperCase()}!`);
+} // trying to get a number in this function will bring me an error.
+
+function getFavoriteNumber(): number {
+  return 26;
+} // with number
+
+function greeting(name: string): void {
+  console.log(`Olá, ${name.toUpperCase()}!`);
+} // when the value is null. its the principle "better explicit than implicit" (that's what she said).
