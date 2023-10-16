e9d8b4a3fda43e9a80aae91ea2fe307d677ef769
juanpinheirx <jroliveirapinheiro@gmail.com>
2023-10-16 05:50:44 -0300

    union type for type combination with example of error

--- day01/index.ts
+++ day01/index.ts
@@ -8,3 +8,9 @@ printId(101);
 printId("202");
 
 // with this, my functin is able to receive a number or string as ID. if i try to get an boolean into this function, i'll get an error from typescript.
+
+function printId(id: number | string) {
+  console.log(id.toUpperCase());
+}
+
+printId(101); // if i try to do the toToUpperCase() method, i'll get an error even before starting my function because this method is just for strings, not numbers as my function requires.
