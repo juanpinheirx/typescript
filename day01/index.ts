750438ed309cf030f9abbddb1bb9d82175569aa4
juanpinheirx <jroliveirapinheiro@gmail.com>
2023-10-16 06:36:33 -0300

    exercises with union types and type aliases and another function

--- day01/index.ts
+++ day01/index.ts
@@ -21,3 +21,15 @@ type RestaurantReserve = {
 const reservation = (data: RestaurantReserve) => {
   return console.log(`${data.person1} and ${data.person2} are expected to be at table 10 tonight. Their ages are: ${data.person1age} and ${data.person2age} respectevely.`);
 } // function that return a reservation using a custom type of information that was create on line 14
+
+type Address = {
+  street: string | number;
+  neighbourhood: string;
+  houseNumber: number;
+  country: string;
+  postalCode: number;
+}
+
+const address = (data: Address) => {
+  return console.log(`This persons address is: street ${data.street} in ${data.neighbourhood}. The house number is ${data.houseNumber}. This address is in ${data.country}. The postal code is: ${data.postalCode}`);
+} // function that return an address with type aliases
